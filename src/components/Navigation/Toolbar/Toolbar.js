8a9df96dffdaa3be305a03388df862f514b799db
Shauna Mack <shaunakatemack@gmail.com>
2020-06-27 22:58:59 +1000

    add navigation items to a toolbar

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import classes from './Toolbar.module.css'
 import Logo from '../../Logo/Logo'
+import NavigationItems from '../NavigationItems/NavigationItems'
 
 function toolbar(props) {
     return (
@@ -9,7 +10,7 @@ function toolbar(props) {
             <div>MENU</div>
             <Logo />
             <nav>
-                ...
+                <NavigationItems />
             </nav>
         </header>
     );
